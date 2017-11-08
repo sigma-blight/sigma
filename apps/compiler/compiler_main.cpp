e25479cda1275e2e37acb64b7859ede673b5d950
Fletcher Blight <fletcher.blight+github@gmail.com>
2017-11-08 16:17:34 +1000

    Initial cmd line parsing

--- apps/compiler/compiler_main.cpp
+++ apps/compiler/compiler_main.cpp
@@ -1,3 +1,9 @@
-int main()
+#include <cmd_line/parser.hpp>
+#include <iostream>
+
+int main(const int argc, const char ** argv)
 {
+	cmd_line::Context context = cmd_line::parse(argc, argv);
+	for (const auto & file : context.source_files)
+		std::cout << file << "\n";
 }
