49739b042662d15a2cbed88c4a03863a95381d85
Fletcher Blight <fletcher.blight+github@gmail.com>
2017-12-08 18:54:15 +1000

    New:

--- apps/compiler/compiler_main.cpp
+++ apps/compiler/compiler_main.cpp
@@ -1,9 +1,31 @@
-#include <cmd_line/parser.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 
 int main(const int argc, const char ** argv)
 {
-	cmd_line::Context context = cmd_line::parse(argc, argv);
-	for (const auto & file : context.source_files)
-		std::cout << file << "\n";
+    // check argc    
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <source_files.sig...>\n";
+        exit(-1);
+    }
+    
+    // check file
+    std::ifstream source_file(argv[1]);
+    if (!source_file) {
+        std::cerr << "Cannot access file: " << argv[1] << "\n";
+        exit(-1);
+    }
+    
+    // start parse
+    std::vector<std::string> code;
+    
+    do { code.emplace_back(); }
+    while (std::getline(source_file, code.back()));
+    code.pop_back();
+              
+    std::copy(code.begin(), code.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 }
